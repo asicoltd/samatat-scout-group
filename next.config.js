d7944e2ab76a395a85157279489f4a61a08e94d1
MD. ASIF HASAN <61615468+asicoltd@users.noreply.github.com>
2026-02-04 21:20:19 +0600

    modified:   package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   images: {
     unoptimized: true,
   },
